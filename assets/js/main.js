bec16ad2155de5317bca41f086e2f0ae1f0a871e
queelgomes <quel.4@hotmail.com>
2023-10-26 22:50:51 -0300

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,52 @@
+const pokemonsHTML = document.querySelector('#pokemons')
+const loadMoreButton = document.querySelector('#loadMoreButton')
+
+
+const maxPokemons = 151
+const limit = 5
+let offset = 0
+
+function loadMorePokemon(offset, limit){
+    pokeApi.getPokemons(offset, limit).then((pokemons = []) => {
+        const newHTML = pokemons.map((pokemon) => 
+            `
+            <li id="pokemon-${pokemon.id}" class="pokemon ${pokemon.typeMain}" onclick="getPokemonChosen(${pokemon.id})"> 
+                <header class="cabecalhoPokemon">
+                    <span class="nomePokemon">${pokemon.name}</span>
+                    <span class="numeroPokemon">#${pokemon.number}</span>
+                </header>
+                <div class="detalhesPokemon">
+                    <ol>
+                        ${pokemon.type.map((tipo) => `<li class="tipoPokemon">${tipo}</li>`).join('')}
+                    </ol>
+                    <img src="${pokemon.photo}" alt="${pokemon.name}">
+                </div>
+            </li>
+            
+            `).join('')
+        
+        pokemonsHTML.innerHTML += newHTML
+    
+    })
+}
+
+loadMorePokemon(offset, limit)
+
+loadMoreButton.addEventListener('click', () => {
+    offset += limit
+    
+    const proximaPaginaPokemons = offset + limit // 15
+
+    if (proximaPaginaPokemons >= maxPokemons) {
+        const novosPokemons = maxPokemons - offset//4
+
+        loadMorePokemon(offset, novosPokemons)
+
+        loadMoreButton.parentElement.removeChild(loadMoreButton)
+    } else {
+        loadMorePokemon(offset, limit)
+    }
+    
+})
+
+
